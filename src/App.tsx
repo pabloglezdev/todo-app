0895fab0a93148496e7a91968a934a3edd3ed72b
Pablo González <pablo.glez.dev@gmail.com>
2026-02-03 22:41:49 +0000

    Config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import './App.css';
-
 function App() {
-  return <div className="text-3xl font-bold underline">Hola</div>;
+  return (
+    <div className="flex flex-row w-screen h-screen p-0">
+      <div className="w-64 bg-mac-sidebar">Sidebar</div>
+      <div className="flex-1">Main Content</div>
+    </div>
+  );
 }
 
 export default App;
